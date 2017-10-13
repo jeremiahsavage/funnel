2885fe7eb68f4eaecf739ed83d74923fefd332c4
Alex Buchanan <buchanae@ohsu.edu>
2017-10-13 10:30:44 -0700

    elastic: add retries with exponential backoff

--- tests/e2e/basic_test.go
+++ tests/e2e/basic_test.go
@@ -394,9 +394,9 @@ func TestMarkCompleteBug(t *testing.T) {
 func TestTaskStartEndTimeLogs(t *testing.T) {
 	id := fun.Run(`--sh 'echo 1'`)
 	task := fun.Wait(id)
-	// Some databases require a small amount of time to process the updates,
+	// Some databases require more time to process the updates,
 	// such as EndTime, which will happen just before fun.Wait() exists above.
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * 500)
 	if task.Logs[0].StartTime == "" {
 		t.Fatal("missing task start time log")
 	}
